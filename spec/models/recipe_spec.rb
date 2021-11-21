c702a276eb40482ff7ca3433e22bae16488b126f
Francesco Perrozzi <francescoperrozzi3@gmail.com>
2021-11-21 12:25:35 -0500

    added rspec validations

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  # Validation tests
+  # ensure columns title and created_by are present before saving
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:ingredients) }
+  it { should validate_presence_of(:instructions) }
 end
